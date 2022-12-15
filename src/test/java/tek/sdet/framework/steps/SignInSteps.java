8f6a61f35ff0f5cb0d7b5ebdcf5d1c7693ec7573
FaisalReshad <faisal.reshad@tekschool.us>
2022-12-15 15:34:26 -0500

    pushing from eclipse after generating new token on github

--- src/test/java/tek/sdet/framework/steps/SignInSteps.java
+++ src/test/java/tek/sdet/framework/steps/SignInSteps.java
@@ -11,7 +11,7 @@ import tek.sdet.framework.utilities.CommonUtility;
 public class SignInSteps extends CommonUtility{
 	private POMFactory factory = new POMFactory();
 	
-	// testing
+	// testingAgain
 	
 	@When("User click on Sign in option")
 	public void userClickOnSignInOption() {
